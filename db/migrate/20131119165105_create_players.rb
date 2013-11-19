016fee0f0bc6243f99a9b238e29b5dfc7cbd674d
samtalks <sam.ikiru@gmail.com>
2013-11-19 14:23:33 -0500

    added yahoo_id to migration and all players to seed file

--- db/migrate/20131119165105_create_players.rb
+++ db/migrate/20131119165105_create_players.rb
@@ -3,6 +3,7 @@ class CreatePlayers < ActiveRecord::Migration
     create_table :players do |t|
       t.string :firstname
       t.string :lastname
+      t.string :yahoo_id
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,91 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20131119174853) do
+
+  create_table "career_stats", force: true do |t|
+    t.integer  "player_id"
+    t.string   "gp"
+    t.string   "mpg"
+    t.string   "fgp"
+    t.string   "threepointp"
+    t.string   "ftp"
+    t.string   "rb"
+    t.string   "ast"
+    t.string   "blk"
+    t.string   "stl"
+    t.string   "turnovers"
+    t.string   "pts"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "game_stats", force: true do |t|
+    t.integer  "player_id"
+    t.string   "date"
+    t.string   "vs"
+    t.string   "score"
+    t.string   "minutes"
+    t.string   "fgam"
+    t.string   "threepointam"
+    t.string   "ftam"
+    t.string   "reb"
+    t.string   "ast"
+    t.string   "blk"
+    t.string   "stl"
+    t.string   "turnovers"
+    t.string   "pts"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "yahoo_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "season_stats", force: true do |t|
+    t.integer  "player_id"
+    t.string   "gp"
+    t.string   "mpg"
+    t.string   "fgp"
+    t.string   "threepointp"
+    t.string   "ftp"
+    t.string   "rb"
+    t.string   "ast"
+    t.string   "blk"
+    t.string   "stl"
+    t.string   "turnovers"
+    t.string   "pts"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "social_links", force: true do |t|
+    t.integer  "player_id"
+    t.string   "twitter"
+    t.string   "youtube"
+    t.string   "facebook"
+    t.string   "tumblr"
+    t.string   "instagram"
+    t.string   "pinterest"
+    t.string   "googleplus"
+    t.string   "weibo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
