5c76510b3d80138b4493397fc9484489336044eb
Theo Vora <theevo@gmail.com>
2013-11-20 10:48:02 -0500

    add search bar

---
+++ app/assets/javascripts/players_search.js
@@ -0,0 +1,29 @@
+$(function() {
+    var availablePlayers = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#players" ).autocomplete({
+      source: availablePlayers
+    });
+  });
