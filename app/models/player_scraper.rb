2c25d7f6cf511c3bd6ebf159238f1a0755208394
jamesjtong <jtong33@gmail.com>
2013-12-19 10:20:40 -0500

    fixes formatting of dates

--- app/models/player_scraper.rb
+++ app/models/player_scraper.rb
@@ -37,7 +37,7 @@ class PlayerScraper
 
   def get_last_game_stats
     @last_game_stats = @player_data.css('tr[class*="team-46"]').map{ |tr|tr.css('td').map(&:text)}
-    @most_recent_0 = {date: @last_game_stats[0][0], vs: @last_game_stats[0][1], score: @last_game_stats[0][2], minutes: @last_game_stats[0][3], fgam: @last_game_stats[0][4], threepointam: @last_game_stats[0][6], ftam: @last_game_stats[0][8], reb: @last_game_stats[0][10], ast: @last_game_stats[0][11], blk: @last_game_stats[0][12], stl: @last_game_stats[0][13], turnovers: @last_game_stats[0][15], pts: @last_game_stats[0][16]}
+    @most_recent_0 = {date: @last_game_stats[0][0], vs: @last_game_stats[0][1].sub('vs', 'vs '), score: @last_game_stats[0][2], minutes: @last_game_stats[0][3], fgam: @last_game_stats[0][4], threepointam: @last_game_stats[0][6], ftam: @last_game_stats[0][8], reb: @last_game_stats[0][10], ast: @last_game_stats[0][11], blk: @last_game_stats[0][12], stl: @last_game_stats[0][13], turnovers: @last_game_stats[0][15], pts: @last_game_stats[0][16]}
     @most_recent_1 = {date: @last_game_stats[1][0], vs: @last_game_stats[1][1], score: @last_game_stats[1][2], minutes: @last_game_stats[1][3], fgam: @last_game_stats[1][4], threepointam: @last_game_stats[1][6], ftam: @last_game_stats[1][8], reb: @last_game_stats[1][10], ast: @last_game_stats[1][11], blk: @last_game_stats[1][12], stl: @last_game_stats[1][13], turnovers: @last_game_stats[1][15], pts: @last_game_stats[1][16]}
     @most_recent_2 = {date: @last_game_stats[2][0], vs: @last_game_stats[2][1], score: @last_game_stats[2][2], minutes: @last_game_stats[2][3], fgam: @last_game_stats[2][4], threepointam: @last_game_stats[2][6], ftam: @last_game_stats[2][8], reb: @last_game_stats[2][10], ast: @last_game_stats[2][11], blk: @last_game_stats[2][12], stl: @last_game_stats[2][13], turnovers: @last_game_stats[2][15], pts: @last_game_stats[2][16]}
     @most_recent_3 = {date: @last_game_stats[3][0], vs: @last_game_stats[3][1], score: @last_game_stats[3][2], minutes: @last_game_stats[3][3], fgam: @last_game_stats[3][4], threepointam: @last_game_stats[3][6], ftam: @last_game_stats[3][8], reb: @last_game_stats[3][10], ast: @last_game_stats[3][11], blk: @last_game_stats[3][12], stl: @last_game_stats[3][13], turnovers: @last_game_stats[3][15], pts: @last_game_stats[3][16]}
@@ -55,7 +55,7 @@ class PlayerScraper
   end
 
   def get_next_game_info
-    @game_time_day = @player_data.css('.time').text.scan(/(\w+)(\d+:\d+\D+)/).flatten
+    @game_time_day = @player_data.css('.time').text.scan(/(\D+)(\d+:\d+\D+)/).flatten
     @game_time = @game_time_day[0]
     @game_day = @game_time_day[1]
     @game_matchup = @player_data.css('.record').text.scan(/([\w\s]+)\(/).join(" vs ")
