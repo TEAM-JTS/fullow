6b6951089f7430cef1d4ec6fc9863493e2eb0f60
jamesjtong <jtong33@gmail.com>
2013-12-31 17:49:33 -0500

    adds discuss and top10 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Fullow::Application.routes.draw do
   root 'players#index'
+
+  get '/top10' => 'players#top10'
+  get '/discuss' => 'players#discuss'
   get '/:slug' => 'players#show', as: 'player'
+
 end
