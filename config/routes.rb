5a596ec72e0c561dd17f3542ae9318fc0c8f2ad4
jamesjtong <jtong33@gmail.com>
2014-02-17 18:50:45 -0500

    adds error handling for nested paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Fullow::Application.routes.draw do
   get '/browse' => 'players#browse'
   get '/top10' => 'players#top10'
   get '/discuss' => 'players#discuss'
-  get '/:slug' => 'players#show', as: 'player'
-
+  get '/*slug' => 'players#show', as: 'player'
 end
